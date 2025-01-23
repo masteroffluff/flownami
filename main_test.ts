ed6d4cef57597208f782a00ab62b8e0899563245
alasdairsmith <alasdairsmith@>
2025-01-23 22:04:21 +0000

    Add express package to deno.json

--- main_test.ts
+++ main_test.ts
@@ -1,4 +1,4 @@
-import { assert, assertStringIncludes } from "@std/assert";
+import { assertStringIncludes } from "@std/assert";
 
 Deno.test("Homepage renders successfully", async () => {
   const response = await fetch("http://localhost:8080/");
