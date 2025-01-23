100179de1083e7bd8d1bfee28d7b4edca1564256
alasdairsmith <alasdairsmith@>
2025-01-23 19:42:24 +0000

    Add EJS for view engine

--- main.ts
+++ main.ts
@@ -3,8 +3,14 @@ import express from "npm:express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the Dinosaur API!");
+app.set("view engine", "ejs");
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get("/", function (req, res) {
+  res.render("pages/index");
 });
 
-app.listen(8000);
+app.listen(8080);
+console.log("Server is listening on port 8080");
