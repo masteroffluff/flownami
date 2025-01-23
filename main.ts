834baf8de010f313dea3684322905aa66e1d8958
alasdairsmith <alasdairsmith@>
2025-01-23 19:14:24 +0000

    Add express

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+// @ts-types="npm:@types/express"
+import express from "npm:express";
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Dinosaur API!");
+});
+
+app.listen(8000);
