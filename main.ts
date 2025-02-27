af6852832c2cca6c1c52eece3cec9ba2ce5d9023
Graham Ashton <gma@users.noreply.github.com>
2025-02-27 19:56:03 +0000

    Display board on /board page (#11)
    Authored-by: willfinnigan (and team)

--- main.ts
+++ main.ts
@@ -5,8 +5,68 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+app.use(express.static("static"));
+
 app.get("/", function (_, res) {
   res.render("pages/index");
+});
+
+type Column = {
+  name: string;
+  tasks: Array<Task>;
+};
+
+type Task = {
+  name: string;
+};
+
+app.get("/board", function (_req, res) {
+  const columns: Array<Column> = [
+    {
+      name: "To Do",
+      tasks: [
+        {
+          name: "Task 1",
+        },
+        {
+          name: "Task 2",
+        },
+        {
+          name: "Task 3",
+        },
+      ],
+    },
+    {
+      name: "Doing",
+      tasks: [
+        {
+          name: "In progress...",
+        },
+      ],
+    },
+    {
+      name: "Done",
+      tasks: [
+        {
+          name: "Finished this one",
+        },
+        {
+          name: "And this one",
+        },
+        {
+          name: "Crikey,",
+        },
+        {
+          name: "Wasn't I...",
+        },
+        {
+          name: "Productive!",
+        },
+      ],
+    },
+  ];
+
+  res.render("pages/board", { columns });
 });
 
 const port = Deno.env.get("PORT") || 8080;
--- main_test.ts
+++ main_test.ts
@@ -5,3 +5,11 @@ Deno.test("Homepage renders successfully", async () => {
   const html = await response.text();
   assertStringIncludes(html, "Flownami");
 });
+
+Deno.test("Board shows three columns", async () => {
+  const response = await fetch("http://localhost:8081/board");
+  const html = await response.text();
+  assertStringIncludes(html, "To Do");
+  assertStringIncludes(html, "Doing");
+  assertStringIncludes(html, "Done");
+});
