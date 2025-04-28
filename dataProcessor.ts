4fc46b395aa36410a0dc11dfcc21f83668c22e9a
Chris Chapman <chris.chapman@linuxmail.org>
2025-04-28 21:43:54 +0100

    deflibulated data

---
+++ dataProcessor.ts
@@ -0,0 +1,33 @@
+import { UUID } from "node:crypto";
+
+type Task = {
+  name: string;
+  id: UUID;
+    column:string;
+};
+
+
+
+type Column = {
+    name: string;
+    tasks: Array<Task>;
+  };
+
+
+  
+async function deflibulateData(){
+    const data = await Deno.readTextFile("./data.json");
+    const columns = JSON.parse(data);
+    const newData:{[key: UUID]:Task} = {};
+    columns.forEach((col: Column) => {
+        const{name, tasks} = col;
+        tasks.forEach(task => {
+            newData[task.id] = {...task, column:name}
+        });
+      });
+
+
+    await Deno.writeTextFile("./new_data.json", JSON.stringify(newData));
+}
+
+deflibulateData();
